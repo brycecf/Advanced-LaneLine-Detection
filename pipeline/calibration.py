106a18f3135b3431762e765ec8e5286049c0fe68
Bryce Freshcorn <bcf4kv@gmail.com>
2018-02-18 19:45:54 -0500

    Update code. Still in-progress.

--- pipeline/calibration.py
+++ pipeline/calibration.py
@@ -4,16 +4,18 @@ import numpy as np
 from os import path
 
 
-def read_chessboard_images(img_dir, include_names=False, color_imgs=False):
+def read_images(img_dir, include_names=False, color_imgs=False, color_mode='BGR'):
     """
     Helper function to read chessboard images to calibrate the camera.
 
     Args:
         img_dir:  Path to the directory containing the chessboard images.
+        include_names:  Return the file names.
+        color_imgs:  Return RGB images.
 
     Returns:
-        If `include_names`, return a list of (File name, Grayscale Image) binaries.  Otherwise,
-        return a list of grayscale image binaries.
+        If `include_names`, return a list of (File name, Image) binaries.  Otherwise,
+        return a list of image binaries.
     """
     img_files = path.join(img_dir, '*.jpg')
     file_names = glob.glob(img_files)
@@ -22,20 +24,23 @@ def read_chessboard_images(img_dir, include_names=False, color_imgs=False):
     elif include_names:
         return [(file_name, cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2GRAY)) for file_name in file_names]
     elif color_imgs:
-        return [cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2RGB) for file_name in file_names]
+        if 'BGR':
+            return [cv2.imread(file_name) for file_name in file_names]
+        else:
+            return [cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2RGB) for file_name in file_names]
     else:
         return [cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2GRAY) for file_name in file_names]
 
 
 def find_chessboard_corners(img_dir, chessboard_size=(9,6), data_for_drawing=False):
     """
-    Locates the chessboard corners from the .
+    Locates the chessboard corners.
 
     Args:
         img_dir:  Path to the directory containing the chessboard images.
 
     Returns:
-        [Object points] and [Image points] lists.
+        [Object points], [Image points], [Grayscale images] lists.
     """
     term_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -48,7 +53,7 @@ def find_chessboard_corners(img_dir, chessboard_size=(9,6), data_for_drawing=Fal
     if data_for_drawing:
         used_imgs_indices = []
 
-    grayscale_imgs = read_chessboard_images(img_dir)
+    grayscale_imgs = read_images(img_dir)
     for i, grayscale_img in enumerate(grayscale_imgs):
         ret, corners = cv2.findChessboardCorners(grayscale_img, chessboard_size)
 
@@ -61,18 +66,46 @@ def find_chessboard_corners(img_dir, chessboard_size=(9,6), data_for_drawing=Fal
                 used_imgs_indices.append(i)
 
     if data_for_drawing:
-        return used_imgs_indices, obj_points, img_points
+        return used_imgs_indices, obj_points, img_points, grayscale_imgs
     return obj_points, img_points
 
 
-def calibrate_camera(img_dir, img_shape, chessboard_size=(9,6)):
-    obj_points, img_points = find_chessboard_corners(img_dir, chessboard_size)
+def calibrate_camera(img_dir, img_shape, chessboard_size=(9,6), data_for_drawing=False):
+    """
+    Calibrates the camera based on chessboard images.
 
+    Args:
+         img_dir:  Path to the directory containing the chessboard images.
+         include_names:  Return the file names.
+         color_imgs:  Return RGB images.
+
+    Returns:
+        Returns the camera matrix, distortion coefficients, rotation and translation vectors,
+        and [Grayscale images].
+    """
+    if data_for_drawing:
+        used_imgs_indices, obj_points, img_points, grayscale_imgs = find_chessboard_corners(img_dir,
+                                                                                            chessboard_size,
+                                                                                            data_for_drawing)
+    else:
+        obj_points, img_points = find_chessboard_corners(img_dir, chessboard_size)
+
+    if data_for_drawing:
+        return cv2.calibrateCamera(obj_points, img_points, img_shape, None, None), grayscale_imgs
     return cv2.calibrateCamera(obj_points, img_points, img_shape, None, None)
 
 
-# def undistort_imgs(img_dir, img_shape, chessboard_size=(9,6)):
-#     ret, matrix, dist, rotation_vecs, translation_vecs = calibrate_camera(img_dir, img_shape, chessboard_size)
-#     matrix, roi = cv2.getOptimalNewCameraMatrix(matrix, dist, img_shape, 1, img_shape)
-#
-#     return
+def undistort_chessboard_imgs(img_dir, img_shape, chessboard_size=(9,6)):
+
+    (ret, matrix, dist, rotation_vecs, translation_vecs), grayscale_imgs = calibrate_camera(img_dir, img_shape,
+                                                                                            chessboard_size, True)
+    new_matrix, roi = cv2.getOptimalNewCameraMatrix(matrix, dist, img_shape, 1, img_shape)
+
+    return [cv2.undistort(img, matrix, dist, None, new_matrix) for img in grayscale_imgs]
+
+
+def undistort_imgs(calibration_dict, img_dir):
+
+    color_imgs = read_images(img_dir, color_imgs=True)
+    return [cv2.undistort(img, calibration_dict['matrix'], calibration_dict['dist'],
+                          calibration_dict['new_matrix']) for img in color_imgs]
